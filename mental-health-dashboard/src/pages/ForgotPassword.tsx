ffae7eb15cf6833581db7e3a01f7928fe6f2fabb
haswitab <hasibokkisam@gmail.com>
2025-08-01 23:13:54 -0400

    css forgot password page to match login/signup page

--- mental-health-dashboard/src/pages/ForgotPassword.tsx
+++ mental-health-dashboard/src/pages/ForgotPassword.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 import { Link } from "react-router";
+import "./auth.css";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
@@ -18,23 +19,23 @@ export default function ForgotPassword() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col justify-center items-center w-xl h-100 px-6 py-12 lg:px-8 max-w-lg border-solid border-2 rounded-lg">
+    <div className="forgot-container">
+      <div className="forgot-box">
         <h1>Reset Password</h1>
         {sent ? (
-          <p className="text-green-600">Password reset email sent!</p>
+          <p className="forgot-sucess-message">Password reset email sent!</p>
         ) : (
           <>
-            <input className="px-5 py-2 w-3/4 my-3 border rounded focus:ring-2 focus:ring-blue-500"
+            <input className="forgot-input-field"
               placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md cursor-pointer"  onClick={handleReset}>Send Reset Email</button>
+            <button className="forgot-submit-button"  onClick={handleReset}>Send Reset Email</button>
           </>
         )}
         <p>
-          <Link className="text-blue-600 underline cursor-pointer hover:text-blue-800" to="/login">Back to login</Link>
+          <Link className="forgot-back-link" to="/login">Back to login</Link>
         </p>
       </div>
     </div>
