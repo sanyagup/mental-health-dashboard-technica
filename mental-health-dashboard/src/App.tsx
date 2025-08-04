68697462ed1e019e28dd7c8c461226eed16c8843
Sanya Gupta <sanyagup07@gmail.com>
2025-08-04 17:16:13 -0400

    updated navbar into component and created + started journaling prompts feature

--- mental-health-dashboard/src/App.tsx
+++ mental-health-dashboard/src/App.tsx
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import ForgotPassword from "./pages/ForgotPassword";
 import MentalHealthTracking from "./pages/MentalHealthTracking";
+import JournalingPrompts from "./pages/JournalingPrompts";
 import "./App.css"
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/MentalHealthTracking" element={user ? <MentalHealthTracking user={user} /> : <Navigate to="login" replace/>}/>
+        <Route path="/JournalingPrompts" element={user ? <JournalingPrompts user={user} /> : <Navigate to="login" replace/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ mental-health-dashboard/src/pages/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import { auth } from "/Users/sanyagupta/Documents/mental-health-dashboard-technica/mental-health-dashboard/src/firebase/firebase.ts";
+import  { signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Ensure you have the correct path to your CSS file
+
+export default function Navbar() {
+
+    const handleLogout = async () => {
+      try {
+        await signOut(auth);
+      } catch (error) {
+        console.error("Error signing out:", error);
+      }
+    };
+
+  return (
+    <div>
+        
+        <nav className="navbar">
+          <div className="navbar-links">
+            <Link to="/" className="navbar-link">
+              Dashboard
+            </Link>
+            <Link to="/MentalHealthTracking" className="navbar-link">
+              Mental Health Tracking
+            </Link>
+            <Link to="/JournalingPrompts" className="navbar-link">
+              Journaling Prompts
+            </Link>
+            <Link to="/profile" className="navbar-link">
+              Profile
+            </Link>
+            <Link to="/settings" className="navbar-link">
+              Settings
+            </Link>
+          </div>
+          <button
+            className="logout-button"
+            onClick={handleLogout}
+          >
+            Log Out
+          </button>
+      </nav>
+    
+    </div>
+  );
+}
